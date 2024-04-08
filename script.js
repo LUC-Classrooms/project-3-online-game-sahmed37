1e97a1ac9ab0ce03d79b66d69ab8096da1f11c94
sahmed37 <156940147+sahmed37@users.noreply.github.com>
2024-04-08 16:17:38 +0000

    project 3 part 0 (almost there)

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
  * 
  * Use this template to get started creating a simple 2D game for the web using P5.js. 
  */
-
+var gameState = "splash";
 function setup() {
 
   createCanvas(600, 400);
@@ -17,7 +17,19 @@ function draw() {
   //splash(); // call the splash screen function (below)
   //play(); // call the play screen function (below)
   //gameOver(); // call the gameOver screen function (below)
-
+switch(gameState){
+  case "splash" : 
+  splash();
+  break; 
+case "play" :
+  play();
+  break;
+case "gameOver" :
+  gameOver();
+  break;
+default :
+ console.log("no match found!"); 
+}
 }
 
 function splash() {
@@ -52,5 +64,12 @@ function gameOver() {
 function mousePressed() {
 
   console.log("click!");
-
+  if(gameState == "splash"){
+    gameState = "play";
+  } else if(gameState == "play"){
+    gameState = "gameover";
+  }else if(gameState == "gameover"){
+    gameState = "splash";
+  }
+  console.log(gameState);
 }
